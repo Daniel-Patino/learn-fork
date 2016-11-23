441b999119ca1c4081a87f4df40f4b626c0fec4b
daniel steven patino <dpati007@AD.CS.FIU.EDU>
2016-11-23 06:59:11 -0500

    Added comment psuedo-code for future

--- main.c
+++ main.c
@@ -2,36 +2,31 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int main()
+int main(int argc, char* argv)
 {
-	pid_t fid1, fid2, fid3;
-	int status;
 	
-	//fid1 = fork();
+	// Here we must check our inputs that will determine how many kids we have
+	// Between 1 and 7, inclusive. Each one will contain a unique integer between 0 - 9
 	
-	fid1 = fork();
-	wait(&status);
+	int pid;
+	int amountOfKids = 5;
+	int processNum;
 	
-	fid1 = fork();
-	wait(&status);
-
-	fid1 = fork();
-	wait(&status);
-	
-	int i = 0;
-	
-	//for(i = 0; i < 3; i++){
-	//	fid1 = fork();
-	//	wait(&status);
-	//}
-	
-	if(fid1 == 0){
-		//wait(&status);
-		printf("This is the Parent\n");
+	// Here we create a loop to create a specified number of children
+	for(processNum = 0; processNum < amountOfKids; processNum++){
+		// Note No error handling just yet
+		pid = fork();
+		if(pid == 0){
+			break;
+		}
 	}
+	
+	// Here we contain the logic for the children and for the parent
+	if(pid == 0){
+		printf("Hello?\n"); // <= These are all the children
+	}
+	
 	else{
-		printf("Hello Children\n");
+		printf("I'm scared.\n"); // <= This is the single parent
 	}
-	
-	return 0;
 }
